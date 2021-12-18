e9639c4fe2041209ac051a06e408e82db6ca0ee1
Bi Wu <alerk_star@outlook.com>
2021-12-19 06:19:14 +0800

    getting quote;

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let currentTrade = {
 };
 let currentSelectSide = "";
 let tokens;
+const activeChain = "eth";
 
 async function login() {
   console.log("[Login]?");
@@ -58,13 +59,39 @@ async function selectToken(address) {
   closeModal();
   currentTrade[currentSelectSide] = tokens[address];
   renderInterface();
+  getQuote();
+}
+
+async function getQuote() {
+  const fromInputValue = $("#from_amount").val();
+  if (
+    !currentTrade.from ||
+    !currentTrade.to ||
+    !fromInputValue ||
+    currentTrade.from.address === currentTrade.to.address
+  )
+    return;
+  const fromAmount = Number(Moralis.Units.ETH(fromInputValue));
+
+  const quote = await Moralis.Plugins.oneInch.quote({
+    chain: activeChain, // The blockchain you want to use (eth/bsc/polygon)
+    fromTokenAddress: currentTrade.from.address, // The token you want to swap
+    toTokenAddress: currentTrade.to.address, // The token you want to receive
+    amount: fromAmount,
+  });
+
+  if (quote.error) {
+    alert(quote.description);
+    return;
+  }
+  $("#to_amount").val(quote.toTokenAmount / 10 ** quote.toToken.decimals);
 }
 
 function renderInterface() {
-  $('#from-token-image').attr('src', currentTrade['from'].logoURI);
-  $('#from-token-symbol').text(currentTrade['from'].symbol);
-  $('#to-token-image').attr('src', currentTrade['to'].logoURI);
-  $('#to-token-symbol').text(currentTrade['to'].symbol);
+  $("#from-token-image").attr("src", currentTrade["from"].logoURI);
+  $("#from-token-symbol").text(currentTrade["from"].symbol);
+  $("#to-token-image").attr("src", currentTrade["to"].logoURI);
+  $("#to-token-symbol").text(currentTrade["to"].symbol);
 }
 
 function openModal(side) {
@@ -82,4 +109,6 @@ $(function () {
   $("#login_button").on("click", login);
   $("#from-token-select").on("click", () => openModal("from"));
   $("#to-token-select").on("click", () => openModal("to"));
+
+  $("#from_amount").on("blur", getQuote);
 });
