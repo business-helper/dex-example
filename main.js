05e3023f793611f563911a26e7bc511bfa8f925f
Bi Wu <alerk_star@outlook.com>
2021-12-19 05:40:03 +0800

    list available tokens;

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 Moralis.initialize("7wLyHqUHZ9RnKvetgr5HhuhCihIJL0fCDHq0CvHC"); // application id from moralis.io
 Moralis.serverURL = "https://hmjmvpi8bepg.usemoralis.com:2053/server"; // server url from moralis.io
 
+let currentTrade = {
+  from: {},
+  to: {},
+};
+let currentSelectSide = '';
+
 async function login() {
   console.log("[Login]?");
   try {
@@ -13,8 +19,65 @@ async function login() {
   }
 }
 
+async function init() {
+  await Moralis.initPlugins();
+  await Moralis.enable();
+  // const tokens = await getSupportedTokens();
+  // console.log('[Tokens]', tokens);
+  listAvailableTokens();
+}
+
+async function getSupportedTokens() {
+  const tokens = await Moralis.Plugins.oneInch.getSupportedTokens({
+    chain: "eth", // The blockchain you want to use (eth/bsc/polygon)
+  });
+  // console.log(tokens);
+  return tokens;
+}
+
+async function listAvailableTokens() {
+  const result = await Moralis.Plugins.oneInch.getSupportedTokens({
+    chain: 'eth',
+  });
+
+  const tokens = result.tokens;
+  let parent = document.getElementById('token-list');
+
+  for (const address in tokens) {
+    let token = tokens[address];
+    let div = document.createElement('div');
+    div.setAttribute('data-address', address);
+    div.className = 'token-row';
+    let html = `
+      <img class="token-list-img" src="${token.logoURI}">
+      <span class="token-list-text">${token.symbol}</span>
+    `;
+
+    div.innerHTML = html;
+    parent.append(div)
+  }
+}
+
+async function selectToken(event) {
+  closeModal();
+  let address = event.target.getAttribute('data-address');
+  console.log('[address]', address);
+  currentTrade
+}
+
+function openModal() {
+  // document.getElementById('token-modal').
+  $('#token-modal').modal('show');
+}
+
+function closeModal() {
+  $('#token-modal').modal('hide');
+}
+
 $(function () {
   console.log("[OnLoading]");
+  init();
+
   $("#login_button").on("click", login);
+  $('#from-token-select').on('click', openModal);
 });
-// document.getElementById('login_button').onclick = login;
